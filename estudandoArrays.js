c0f567ac6e9412ba30ac02cbffc80a5af33f1fd4
Bianca Motta <biancabmotta@gmail.com>
2022-03-15 16:15:08 -0400

    menu

--- estudandoArrays.js
+++ estudandoArrays.js
@@ -5,55 +5,83 @@ let lista = [];
 
 lista.push(10, 20, 30, 40);
 
-console.log(lista);
+let op= null;
+while (op != 0) {
 
-function listarElementos() {
+    console.log("MENU DE OPÇÕES\n"
+        + "1: Imprimir lista.\n"
+        + "2: Imprimir lista como 'índice - elemento'.\n"
+        + "3: Calcular média.\n"
+        + "4: Excluir valores.\n"
+        + "5: Adicionar um valor.\n"
+        + "0: Sair.\n");
 
-    console.log('Esta lista possui os elementos "')
-    for (let i = 0; i < lista.length; i++) {
-        if (i != lista.length - 1) {
+    op = parseInt(readlineSync.question("Escolha o número referente a ação desejada dentre as opções do MENU:"));
 
-            if (i != lista.length-2){
-                console.log(lista[i] + ", ");
-            }else{
-                console.log(lista[i] + " ");
+    executarMenu(op);
+}
+
+//FUNÇÕES//
+
+function executarMenu() {
+    switch (op) {
+        case 1:
+            console.log(lista);
+            break;
+        case 2:
+            listarElementos();
+            break;
+        case 3:
+            calcularMedia(lista);
+            break;
+        case 4:
+            excluirElementos();
+            break;
+        case 5:
+            adicionarElementos();
+            break;
+        case 0:
+            op = 0;
+            break;
+        default:
+            console.log("Digite uma opção válida.");
+            break;
+    }
+}
+
+    function listarElementos() {
+
+        console.log("Esta lista possui os elementos ")
+        for (let i = 0; i < lista.length; i++) {
+
+            console.log("Índice " + i + ": " + lista[i]);
+        }
+    }
+
+    function calcularMedia(lista) {
+
+        let soma = 0;
+        for (let i = 0; i < lista.length; i++) {
+            soma = soma + lista[i];
+        }
+        media = soma / lista.length;
+
+        console.log("A média é: " + media);
+    }
+
+    function excluirElementos() {
+
+        const elemento = parseInt(readlineSync.question("Informe o valor que deseja remover:"));
+        for (let i = 0; i < lista.length; i++) {
+            if (lista[i] == elemento) {
+                lista.splice(i, 1);
             }
-        } else {
-            console.log("e " + lista[i] + '".');
         }
     }
-}
-//console.log(listarElementos);
 
-function calcularArea(lista) {
+    function adicionarElementos() {
 
-    let soma = 0;
-    for (let i = 0; i < lista.length; i++) {
-        soma = soma + lista[i];
+        const elemento = parseInt(readlineSync.question("Informe um valor para ser adicionada ao fim da lista: "));
+        lista.push(elemento);
+
     }
-    media = soma / lista.length;
-
-    return media
-}
-//console.log(calcularArea(lista));
-
- function excluirElementos(){
-    
-    const elemento = parseInt(readlineSync.question("Informe o valor que deseja remover "));
-    for (let i = 0; i < lista.length; i++) {
-        if (lista[i] == elemento){
-            lista.splice(i,1);
-        }
-    }
- }
-//   excluirElementos();
-//   console.log(lista);
-
-function adicionarElementos(){
-
-    const elemento = parseInt(readlineSync.question("Informe um valor para ser adicionada ao fim da lista: "));
-    lista.push(elemento);
-
-}
-//  adicionarElementos();
-//  console.log(lista);
